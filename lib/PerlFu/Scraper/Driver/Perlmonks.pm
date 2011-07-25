231ed31b125602a5d8a462fe60e9f75938dd9202
Devin Austin <devin.austin@gmail.com>
2011-07-25 13:40:06 -0600

    looks like we're retrieving a good number now

--- lib/PerlFu/Scraper/Driver/Perlmonks.pm
+++ lib/PerlFu/Scraper/Driver/Perlmonks.pm
@@ -3,20 +3,14 @@ package PerlFu::Scraper::Driver::Perlmonks;
 use Moose::Role;
 use namespace::autoclean;
 use WWW::Mechanize;
-use Data::Feed;
-
-has 'www' => (
-  is       => 'ro',
-  lazy     => 1,
-  required => 1,
-  default => sub { WWW::Mechanize->new },
-);
+use XML::Feed;
 
 sub scrape_action {
   my ($self, $uri) = @_;
   my $rss;
   my @entries;
-  $rss = Data::Feed->parse($uri);
+  $rss = XML::Feed->parse($uri)
+    or die XML::Feed->errstr;
   @entries = $rss->entries;
   return \@entries
   
--- script/perlmonks_scraper.pl
+++ script/perlmonks_scraper.pl
@@ -4,6 +4,12 @@ use PerlFu::Scraper;
 use Data::Dumper;
 use URI;
 my $scraper = PerlFu::Scraper->new( driver => "perlmonks" );
-my $res = $scraper->scrape_it(URI->new("http://perlmonks.org/?node_id=30175;xmlstyle=rss;types=perlquestion"));
-
-print Dumper $res;
+my $res = $scraper->scrape_it(URI->new("http://perlmonks.org/?node_id=30175;xmlstyle=rss"));
+my $num = 0;
+for $entry ( @{$res} ) {
+  print "Format: " . $entry->format;
+  print "Dumper: " . Dumper $entry;
+  print "title: " . $entry->title . "\n";
+  print "Entry: $num\n";
+  $num++;
+}
