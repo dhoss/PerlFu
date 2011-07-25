4db6c5351a45ee49abe3ded1f037501281176694
Devin <devin@Abominus.(none)>
2011-07-25 12:54:49 -0600

    updated driver

--- lib/PerlFu/Scraper/Driver/Perlmonks.pm
+++ lib/PerlFu/Scraper/Driver/Perlmonks.pm
@@ -2,14 +2,22 @@ package PerlFu::Scraper::Driver::Perlmonks;
 
 use Moose::Role;
 use namespace::autoclean;
-
+use WWW::Mechanize;
 use Data::Feed;
+
+has 'www' => (
+  is       => 'ro',
+  lazy     => 1,
+  required => 1,
+  default => sub { WWW::Mechanize->new },
+);
 
 sub scrape_action {
   my ($self, $uri) = @_;
-
-  my $rss = Data::Feed->parse($uri);
-  my @entries = $rss->entries;
+  my $rss;
+  my @entries;
+  $rss = Data::Feed->parse($uri);
+  @entries = $rss->entries;
   return \@entries
   
 }
