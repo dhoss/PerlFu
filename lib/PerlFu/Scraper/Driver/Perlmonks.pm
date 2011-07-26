220c6da0ba45047af5ff58aa4f9defad7ad63e6c
Devin Austin <devin.austin@gmail.com>
2011-07-25 21:56:36 -0600

    first swipe at gathering/parsing

--- lib/PerlFu/Scraper/Driver/Perlmonks.pm
+++ lib/PerlFu/Scraper/Driver/Perlmonks.pm
@@ -4,20 +4,55 @@ use Moose::Role;
 use namespace::autoclean;
 use WWW::Mechanize;
 use XML::Feed;
-use Path::Class;
+use XML::Twig;
+use File::Find::Rule;
+# needs to go in a BB container
+use ElasticSearch;
+
+has 'es' => (
+  is => 'ro',
+  required => 1,
+  default => sub {
+    ElasticSearch->new(
+      servers      => 'localhost:9200',
+      transport    => 'http',                  # default 'http'
+      max_requests => 10_000,                 # default 10_000
+      trace_calls  => 'log_file',
+      no_refresh   => 0 ,
+    );
+  }
+);
+
+has 'twig' => (
+  is       => 'ro',
+  lazy     => 1,
+  required => 1,
+  default  => sub {
+    XML::Twig->new;
+  },
+);
 
 sub scrape_action {
   my ( $self, $uri, $opts ) = @_;
   my $rss;
   my @entries;
   if ( defined $opts->{directory} ) {
-    warn "Dir: " . $uri ;
-    my $dir = dir $uri;
-    my $handle = $dir->open;
-    while ( my $file = $handle->read ) {
-      $file = $dir->file($file);
+    print "Dir: " . $uri . "\n";
+    print "Collecting files...\n";
+    my @files = File::Find::Rule->file()->name("*.xml")->in($uri);
+    for my $file ( @files ) {
+      my $t = $self->twig;
       print "File name: $file\n";
-      print "File contents: " . $file->slurp . "\n";
+      print "Parsing...\n";
+      $t->parsefile($file) 
+        or die "Can't parse $file: $!";
+      print "Indexing...\n";
+      #$self->es->index(
+      #  index => 'perlfu',
+      #  type  => 'perlmonks_node',
+      #  data  => {
+      #  }
+      #);
     }
   } else {
     $rss = XML::Feed->parse($uri)
