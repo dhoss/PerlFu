060f670d216a39606b18e29ca0a20e734fac5b29
Devin Austin <devin.austin@gmail.com>
2011-08-03 13:42:35 -0600

    added preliminary app cmd stuff

---
+++ lib/PerlFu/Cmd/Command/load_xml.pm
@@ -0,0 +1,45 @@
+package PerlFu::Cmd::Command::load_xml;
+
+use 5.12.0;
+use Moose;
+use namespace::autoclean;
+use Path::Class qw( dir file );
+use XML::Toolkit::Loader;
+
+extends qw(MooseX::App::Cmd::Command);
+
+has dir => (
+  is            => 'rw',
+  isa           => 'Str',
+  traits        => [qw( Getopt )],
+  required      => 1,
+  cmd_aliases   => 'd',
+  documentation => "specify a directory to grab XML files from.",
+);
+
+has loader => (
+  is      => 'ro',
+  isa     => 'XML::Toolkit::Loader',
+  traits  => [qw( NoGetopt )],
+  builder => '_build_loader',
+  lazy    => 1,
+);
+
+sub execute {
+  my ( $self, $opt, $args ) = @_;
+  say "Beginning processing";
+  my $dir_handle = dir($self->dir)->open
+    or die "Can't open " . $self->dir->stringify . " : $!";
+  while ( my $file = $dir_handle->read ) {
+    say "Processing $file";
+  }
+
+}
+
+sub _build_loader {
+  my $self = shift;
+  return XML::Toolkit::Loader->new;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
---
+++ script/perlfu_ctl.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+use strict;
+use PerlFu::Cmd;
+my $app = PerlFu::Cmd->new;
+$app->run;
