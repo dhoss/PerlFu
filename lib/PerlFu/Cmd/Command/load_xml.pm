317fa75c18c9f0e83dc69b2f0726e9e62a89602a
Devin Austin <devin.austin@gmail.com>
2011-08-03 14:27:35 -0600

    okay, error catching works, object instantiation works

--- lib/PerlFu/Cmd/Command/load_xml.pm
+++ lib/PerlFu/Cmd/Command/load_xml.pm
@@ -3,8 +3,9 @@ package PerlFu::Cmd::Command::load_xml;
 use 5.12.0;
 use Moose;
 use namespace::autoclean;
-use Path::Class qw( dir file );
-use XML::Toolkit::Loader;
+use File::Find::Rule;
+use XML::Toolkit::App;
+use Try::Tiny;
 
 extends qw(MooseX::App::Cmd::Command);
 
@@ -17,29 +18,41 @@ has dir => (
   documentation => "specify a directory to grab XML files from.",
 );
 
-has loader => (
+has app_loader => (
   is      => 'ro',
-  isa     => 'XML::Toolkit::Loader',
+  isa     => 'XML::Toolkit::App',
   traits  => [qw( NoGetopt )],
-  builder => '_build_loader',
+  builder => '_build_app_loader',
   lazy    => 1,
 );
 
 sub execute {
   my ( $self, $opt, $args ) = @_;
   say "Beginning processing";
-  my $dir_handle = dir($self->dir)
-    or die "Can't open " . $self->dir->stringify . " : $!";
-  while ( my $file = $dir_handle->next ) {
+  my $dir =  $self->dir;
+  my $xml_loader = $self->app_loader->loader;
+  my @files = File::Find::Rule->file()
+                              ->name('*.xml')
+                              ->in($dir);
+  for my $file ( @files ) {
     say "Processing $file";
+    say "Creating XML::Toolkit object";
+    try {
+      $xml_loader->parse_file($file) 
+        or die "Error parsing: $!";
+    } catch {
+      say "Whoops, couldn't parse $file: $_";
+    };
+    say "Completed processing";
+
 
   }
 
 }
 
-sub _build_loader {
+sub _build_app_loader {
   my $self = shift;
-  return XML::Toolkit::Loader->new;
+  return XML::Toolkit::App->new( xmlns => { '' => 'PerlFu::XML' } );
 }
 
 __PACKAGE__->meta->make_immutable;
