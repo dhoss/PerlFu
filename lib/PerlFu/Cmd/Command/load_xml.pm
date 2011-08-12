34efd86f80ab759c78ce068c58b96da1efa8b2a5
Devin Austin <devin.austin@gmail.com>
2011-08-12 15:40:15 -0600

    switched to ::CompactTree

--- lib/PerlFu/Cmd/Command/load_xml.pm
+++ lib/PerlFu/Cmd/Command/load_xml.pm
@@ -4,11 +4,12 @@ use 5.12.0;
 use Moose;
 use namespace::autoclean;
 use File::Find;
-use XML::Toolkit::App;
 use Try::Tiny;
 use ElasticSearch;
 use Data::Page;
-
+use XML::CompactTree::XS;
+use XML::LibXML::Reader;
+use Data::Dumper;
 extends qw(MooseX::App::Cmd::Command);
 
 has dir => (
@@ -22,13 +23,6 @@ has dir => (
   default       => ".",
 );
 
-has app_loader => (
-  is      => 'ro',
-  isa     => 'XML::Toolkit::App',
-  traits  => [qw( NoGetopt )],
-  builder => '_build_app_loader',
-  lazy    => 1,
-);
 
 has 'es' => (
   is       => 'ro',
@@ -50,6 +44,7 @@ has 'pager' => (
   }
 );
 
+
 sub execute {
   my ( $self, $opt, $args ) = @_;
   my $dir = $self->dir;
@@ -57,15 +52,18 @@ sub execute {
   my @files;
   find( sub { push @files, $File::Find::name if /\.xml$/ }, $dir );
   say "Building data structure...";
-  my $pages = $self->pager;
+  my $pages  = $self->pager;
   my @parsed = $self->build_bulk_data( \@files );
-  $pages->total_entries(scalar @parsed);
+  say "Done parsing...";
+  $pages->total_entries( scalar @parsed );
+  say "Parsed " . scalar @parsed . " files";
   say "Indexing...";
   my $result;
+
   for ( $pages->next_page ) {
     my $records = $pages->splice( \@parsed );
     say "****Page: " . $pages->current_page;
-    $result = $self->es->bulk_index( $records );
+    #$result = $self->es->bulk_index($records);
     say "Indexed successfully";
   }
 
@@ -75,38 +73,42 @@ sub build_bulk_data {
   my ( $self, $files ) = @_;
   my @file_list = @{$files};
   my @bulk_data;
-  my $xml_loader = $self->app_loader->loader;
-
+  my $counter = 0;
   for my $file (@file_list) {
     try {
-      say "Parsing $file";
-      open my $fh, "<",  $file or die "can't open $file :$!";
+      $counter += 1;
+      say "Parsing $file ($counter of " . scalar @file_list . " files)" . ($counter/(scalar @file_list)) . "% complete";
+      open my $fh, "<", $file or die "can't open $file :$!";
       my $contents = do { local $/; <$fh> };
       close $fh;
-      eval { $xml_loader->parse_string($contents) };
-      next if $@;
-      my $root = $xml_loader->root_object;
-      if ( $root->title =~ /(^Permission Denied|user image)/g ) {
-        warn "Title: " . $root->title;
-        warn "Node ID: " . $root->id;
-        next;
-      }
-      my $data = @{ $root->data_collection }[0];
-      push @bulk_data,
-      {
-        index => 'perlmonks',
-        type  => 'perlmonks_node',
-        data  => {
-          node => {
-            title   => $root->title,
-            author  => @{ $root->author_collection }[0]->text,
-            id      => $root->id,
-            content => @{ $data->field_collection }[0]->text,
-            created => $root->created,
-            updated => $root->updated,
-          }
-        }
-      };
+      say "Read";
+      my $reader = XML::LibXML::Reader->new( string => $contents ) 
+        || die "can't read file $!";
+      say "Read in contents";
+      say "creating tree";
+      my $tree = XML::CompactTree::XS::readSubtreeToPerl($reader, XCT_DOCUMENT_ROOT|XCT_IGNORE_WS|XCT_IGNORE_COMMENTS);
+      say "Set contents";
+      #if ( $root->title =~ /(^Permission Denied|user image)/g ) {
+      #  warn "Title: " . $root->title;
+      #  warn "Node ID: " . $root->id;
+      #  next;
+      #}
+      #my $data = @{ $root->data_collection }[0];
+      push @bulk_data, $tree;
+      #  {
+      #  index => 'perlmonks',
+      #  type  => 'perlmonks_node',
+      #  data  => {
+      #    node => {
+      #      title   => $root->title,
+      #      author  => @{ $root->author_collection }[0]->text,
+      #      id      => $root->id,
+      #      content => @{ $data->field_collection }[0]->text,
+      #      created => $root->created,
+      #      updated => $root->updated,
+      #    }
+      #  }
+      #  };
 
     }
     catch {
@@ -114,11 +116,6 @@ sub build_bulk_data {
     };
   }
   return \@bulk_data;
-}
-
-sub _build_app_loader {
-  my $self = shift;
-  return XML::Toolkit::App->new( xmlns => { '' => 'PerlFu::XML' } );
 }
 
 __PACKAGE__->meta->make_immutable;
