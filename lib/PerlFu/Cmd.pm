3967aacac29d4d43e29954afabab4b61254e6698
Devin Austin <devin.austin@gmail.com>
2011-09-08 22:14:38 -0600

    added ABSTRACT to perlfu::cmd

--- lib/PerlFu/Cmd.pm
+++ lib/PerlFu/Cmd.pm
@@ -3,6 +3,6 @@ package PerlFu::Cmd;
 use Moose;
 use namespace::autoclean;
 extends qw(MooseX::App::Cmd);
-
+# ABSTRACT: base class for perlfu's CLI operations
 __PACKAGE__->meta->make_immutable;
 1;
